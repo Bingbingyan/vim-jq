68f5008c2e82410f0b2756be7415664de03dce73
Bing.B.Yan <Bing.B.Yan@yzw.cn>
2018-04-14 16:31:46 +0800

    Json 格式失败时，自动跳转到 JSON 错误的位置

--- plugin/vim-jq.py
+++ plugin/vim-jq.py
@@ -21,15 +21,11 @@ def formatjson_from_buffer():
         res = formatjson(block)
         win.buffer[:] = res.split('\n')
     except Exception as e:
-        # m = re.findall(r'line\s\d*\scolumn\s\d*', e)
-        # print(m)
-        # if len(m) > 0:
-            # location = m[0].split(' ')
-            # vim.command('cal cursor( %s %s)' % (location[1], location[3]))
+        m = re.findall(r'line\s\d*\scolumn\s\d*', str(e))
+        if m and len(m) > 0:
+            location = m[0].split(' ')
+            vim.command('cal cursor(%s,%s)' % (location[1], location[3]))
         print(e)
-
-        # TODO: 自动跳转到格式失败的位置
-        # Expecting property name enclosed in double quotes: line 5 column 13 (char 47)
 
 
 def formatjson(block):
